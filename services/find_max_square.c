db162365eeae84ebf4b781cf9d973d829f8c7004
Noppadon Piya-isarakul <npiya-is@z2t17c2.42bangkok.com>
2021-12-15 14:53:19 +0700

    final

--- services/find_max_square.c
+++ services/find_max_square.c
@@ -6,7 +6,7 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 12:35:50 by npiya-is          #+#    #+#             */
-/*   Updated: 2021/12/15 09:04:11 by tsomsa           ###   ########.fr       */
+/*   Updated: 2021/12/15 14:53:06 by npiya-is         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,10 @@
 #include <stdio.h>
 #include "../headers/types.h"
 
-
+int	min_size(t_square sq, t_board b);
+int	expand(t_square sq, t_board b);
 int			is_fillable_square(t_square square, t_board board);
-t_square	able_to_fill(t_square square, t_board board);
+int	able_to_fill(t_square square, t_board board);
 t_board	set_max_square(t_square square, t_board board);
 
 t_board	find_max_square(t_board board)
@@ -26,25 +27,29 @@ t_board	find_max_square(t_board board)
 	t_square		temp_sq;
 
 	i = 0;
-	j = 0;
-	temp_sq.len = 0;
 	board.sq.len = 0;
+	temp_sq.len = 0;
 	while (i < board.height)
 	{
-		temp_sq.x0 = i;
-		j = 0;	
+		j = 0;
 		while (j < board.width)
 		{
+			temp_sq.x0 = i;
 			temp_sq.y0 = j;
-			temp_sq = able_to_fill(temp_sq, board);
-			if (temp_sq.len > board.sq.len)
-				board = set_max_square(temp_sq, board);
+			if (able_to_fill(temp_sq, board))
+			{
+				temp_sq.len = able_to_fill(temp_sq, board);
+				if (temp_sq.len > board.sq.len)
+					board = set_max_square(temp_sq, board);
+				temp_sq.len = 0;
+			}
 			j++;
 		}
 		i++;
 	}
+	printf("return x0 : %d , y0 : %d, len ; %d\n",board.sq.x0, board.sq.y0, board.sq.len);
 	return (board);
-}
+}	
 
 t_board	set_max_square(t_square square, t_board board)
 {
@@ -54,103 +59,48 @@ t_board	set_max_square(t_square square, t_board board)
 	return (board);
 }
 
-t_square	able_to_fill(t_square square, t_board board)
+int	able_to_fill(t_square sq, t_board b)
+{
+	if (sq.x0 < b.height - 1 && sq.y0 < b.width - 1)
+	{
+		if (min_size(sq, b))
+		{
+			sq.len = 2;
+			sq.len = expand(sq, b);
+			return (sq.len - 1);
+		}
+	}
+	return (0);	
+}
+
+int	min_size(t_square sq, t_board b)
+{
+	if (b.data[sq.x0][sq.y0] != b.obs && b.data[sq.x0 + 1][sq.y0] != b.obs
+			&& b.data[sq.x0][sq.y0 + 1] != b.obs 
+				&& b.data[sq.x0 + 1][sq.y0 + 1] != b.obs)
+					return (1);
+	return (0);
+}
+
+int	expand(t_square sq, t_board b)
 {
 	int	i;
 	int	j;
 	int	k;
-	int	len;
-
-	i = square.x0;
-	j = square.y0;
-	k = 0;
-	len = 0;
-	// printf("start point row : %d col : %d len : %d\n",row, col, square.len);
-	if (board.data[i][j] == board.obs)
-	{	square.len = 0;
-		return (square);
-	}
-	if (!square.len)
-	{
-		while (board.data[i][j] != board.obs && j < board.width)
-		{
-			len++;
-			j++;
-		}
-		square.len = len;
-	}
-	else
-	{
-		while (k < square.len && j < board.width)
-		{
-			k++;
-			j++;
-		}
-	}
-	if (square.len == 0)
-		return (square);
-	j--;
-	k = 1;
-	//printf("move1 row : %d, col : %d len : %d\n", row, col, square.len); 
-	while ( k < square.len && i < board.height - 1)
-	{
-		if (board.data[i][j] == board.obs)
-		{
-			square.len--;
-			return (able_to_fill(square, board));
-		}
-		i++;
-		k++;
-	}
-	// printf("move2 row row : %d, col : %d len : %d\n", row, col, square.len); 
-	k = 1;
-	while (k < square.len)
-	{	
-		if (board.data[i][j] == board.obs)
-		{
-			square.len--;
-			return (able_to_fill(square, board));
-		}
-		j--;
-		k++;
-	}
-	k = 1;
-	// printf("move3 col row : %d, col : %d len : %d\n", i, j, square.len); 
-	while (k < square.len)	
-	{		
-		if (board.data[i][j] == board.obs)
-		{
-			square.len--;
-			return (able_to_fill(square, board));
-		}
-		i--;
-		k++;
-	}
-	if (!is_fillable_square(square, board))
-	{
-		square.len--;
-		return (able_to_fill(square, board));
-		// printf(" last move col row : %d, col : %d len : %d\n", row, col, square.len);
-	}
-	return (square);
-}
-
-int	is_fillable_square(t_square square, t_board board)
-{
-	int	i;
-	int	j;
 
 	i = 0;
-	while (i < square.len)
+	j = sq.y0 + i;
+	k = sq.x0 + i;
+	if (sq.x0 + sq.len == b.height  || sq.y0 + sq.len == b.width)
+		return (sq.len);
+	while (i <= sq.len && (i + sq.len) < b.height)
 	{
-		j = 0;
-		while (j < square.len)
-		{
-			if (board.data[i][j] == board.obs)
-				return (0);
-			j++;
-		}
+		if (b.data[sq.x0 + sq.len][sq.y0 + i] == b.obs)
+			return (sq.len);
+		if (b.data[sq.x0 + i][sq.y0 + sq.len] == b.obs)
+			return (sq.len);
 		i++;
 	}
-	return (1);
+	sq.len++;
+	return (expand(sq, b));
 }

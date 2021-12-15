b3006381f9e715305be14a75e35691810732a594
Thitiwut Somsa <tsomsa@z1t13c3.42bangkok.com>
2021-12-16 01:53:05 +0700

    fix norm

--- headers/utils.h
+++ headers/utils.h
@@ -6,7 +6,7 @@
 /*   By: tsomsa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 10:15:08 by tsomsa            #+#    #+#             */
-/*   Updated: 2021/12/15 19:54:27 by tsomsa           ###   ########.fr       */
+/*   Updated: 2021/12/16 01:51:48 by tsomsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int		is_c_printable(char c);
 void	char_write(char *c);
 char	*str_copy(char *dest, char *src);
 char	*str_concat(char *dest, char *src);
-
 
 t_file	read_file(char *filename, t_file file);
 
--- utilities/read_file.c
+++ utilities/read_file.c
@@ -6,7 +6,7 @@
 /*   By: npiya-is <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 22:16:50 by npiya-is          #+#    #+#             */
-/*   Updated: 2021/12/16 01:17:29 by tsomsa           ###   ########.fr       */
+/*   Updated: 2021/12/16 01:52:48 by tsomsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ t_file	read_file(char *filename, t_file file)
 	file.size = read_file_size(file);
 	close_file(file);
 	file = open_file(file);
-	file.data = malloc((file.size + 1)  * sizeof(char));
+	file.data = malloc((file.size + 1) * sizeof(char));
 	file = read_buffer(file);
 	file = close_file(file);
 	return (file);
@@ -52,7 +52,7 @@ t_file	open_file(t_file file)
 	return (file);
 }
 
-int		read_file_size(t_file file)
+int	read_file_size(t_file file)
 {
 	char	*buf;
 	int		ret;

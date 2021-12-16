ea30ec8f64155ca3db22026a1119842c058618ba
Thitiwut Somsa <tsomsa@z1t13c3.42bangkok.com>
2021-12-16 15:54:01 +0700

    update gen and test

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 RED="\033[1;31m"
 RESET="\033[m"
 main=test.c
+filename="assets/fun_board.txt"
+
 if [[ -z $1 && -z $2 && -z $3 ]]
 then
 	width=20
@@ -13,16 +15,19 @@ else
 	dens=$3
 fi
 
-if [ -z $4]
-then
-	filename="assets/fun_board.txt"
-else
-	filename=$4
-fi
-
 echo "${RED}Generate assets/fun_board.txt${RESET}"
-perl generate_fun_board.pl $width $height $dens> assets/fun_board.txt
+if [[ -z $4 && -z $5 && -z $6 ]]
+then
+	echo "normal map"
+	perl generate_fun_board.pl $width $height $dens > $filename 
+else
+	empty=$4
+	obs=$5
+	full=$6
+	perl generate_fun_board.pl $width $height $dens $empty $obs $full > $filename 
+fi
 cat $filename
+
 echo "";
 
 gcc -Wall -Wextra -Werror $main services/*.c utilities/*.c -o runner.out
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: tsomsa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 01:38:41 by tsomsa            #+#    #+#             */
-/*   Updated: 2021/12/16 05:56:03 by tsomsa           ###   ########.fr       */
+/*   Updated: 2021/12/16 15:03:35 by tsomsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -14,26 +14,18 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include "headers/utils.h"
-#include "headers/types.h"
+#include "headers/constants.h"
 #include "headers/services.h"
 
 int	main(void)
 {
-	t_board	board;
-	t_file	file;
+	char	**arr;
 
-	file.desc = 0;
-	board.width = 0;
-	file = read_file("assets/fun_board.txt", file);
-	file = validate_map_header_file(file);
-	if (file.desc == -1)
-		printf("%s\n", file.msg);
-	board = set_board_desc(file, board);
-	board = validate_board_data(file, board);
-	board = get_fun_board(file, board);
-	free(file.data);
-	board = find_max_square(board);
-	print_fun_board(board);
-	free(board.data);
+	arr = malloc(2 * 100 * sizeof(char));
+	arr[1] = malloc(100 * sizeof(char));
+	arr[1] = str_copy(arr[1], "assets/fun_board.txt");
+	runner(2, arr);
+	free(arr);
+	str_print_color("== Finish!!  ==\n", C_RED);
 	return (0);
 }

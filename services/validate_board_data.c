5050ed8c873318e4218bb71319c175623fb88db1
Thitiwut Somsa <tsomsa@z1t13c3.42bangkok.com>
2021-12-16 18:20:43 +0700

    fix null line

--- services/validate_board_data.c
+++ services/validate_board_data.c
@@ -6,7 +6,7 @@
 /*   By: tsomsa <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 22:47:06 by tsomsa            #+#    #+#             */
-/*   Updated: 2021/12/16 04:04:25 by tsomsa           ###   ########.fr       */
+/*   Updated: 2021/12/16 18:11:40 by tsomsa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdlib.h>
@@ -24,6 +24,11 @@ t_board	validate_board_data(t_file file, t_board board)
 	line = count_line(file.data);
 	board.is_valid = 0;
 	board.msg = malloc(100 * sizeof(char));
+	if (line < 1)
+	{
+		board.msg = str_copy(board.msg, "MAP ERROR: need header and more than 1 file");
+		return (board);
+	}
 	if (line != board.height)
 	{
 		board.msg = str_copy(board.msg, "MAP ERROR: Height is mismatch");
@@ -86,7 +91,7 @@ int	is_same_line_length(char *str)
 			count++;
 			str++;
 		}
-		if (count != width)
+		if (count == 0 || count != width)
 			return (0);
 		str++;
 	}
